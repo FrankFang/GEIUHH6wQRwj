84555d668a88cfd819ed1467134f67e0bce69641
frankfang <frankfang1990@gmail.com>
2020-07-30 02:48:43 +0800

    switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -24,7 +24,7 @@ export default {
     height: $h;
     width: $h*2;
     border: none;
-    background: grey;
+    background: #bfbfbf;
     border-radius: $h/2;
     position: relative;
   }
@@ -36,15 +36,21 @@ export default {
     width: $h2;
     background:white;
     border-radius: $h2 / 2;
-    transition: left 250ms;
+    transition: all 250ms;
   } 
   button.checked{
-    background: blue;
+    background: #1890ff;
   }
   button.checked > span {
     left: calc(100% - #{$h2} - 2px);
   }
   button:focus{
     outline: none;
+  }
+  button:active{
+    > span {width: $h2 + 4px;}
+  }
+  button.checked:active{
+    > span {width: $h2 + 4px; margin-left: -4px;}
   }
 </style>
