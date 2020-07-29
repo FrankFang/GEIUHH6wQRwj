e7f70de4b46e0a41459266b0690e234b05eaa135
frankfang <frankfang1990@gmail.com>
2020-07-30 01:32:44 +0800

    Switch css 1

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -3,7 +3,23 @@
 </template>
 
 <style lang="scss" scoped>
+  $h: 22px;
+  $h2: $h - 4px;
+  button{
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: blue;
+    border-radius: $h/2;
+    position: relative;
+  }
   span{
-
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background:white;
+    border-radius: $h2 / 2;
   } 
 </style>
