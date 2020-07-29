bbfd60f5493371fbdaa793099025116969baea4d
frankfang <frankfang1990@gmail.com>
2020-07-30 01:47:12 +0800

    动画流畅了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -33,11 +33,15 @@ export default {
     width: $h2;
     background:white;
     border-radius: $h2 / 2;
+    transition: left 250ms;
   } 
   button.checked{
     background: blue;
   }
   button.checked > span {
     left: calc(100% - #{$h2} - 2px);
+  }
+  button:focus{
+    outline: none;
   }
 </style>
