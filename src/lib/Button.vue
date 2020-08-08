90176c7ebcf4738488521eefc7cdd0096a97c510
frankfang <frankfang1990@gmail.com>
2020-08-09 05:09:00 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -47,5 +47,22 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.gulu-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.gulu-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+
+  }
 }
 </style>
