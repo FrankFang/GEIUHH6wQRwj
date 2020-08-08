5be561005a50496e774637db9a106f3bdd8b6f2b
frankfang <frankfang1990@gmail.com>
2020-08-09 03:39:41 +0800

    基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,7 +1,5 @@
 <template>
-  <button class="gulu-button"
-    :class="{[`theme-${theme}`]: theme}"
-  >
+  <button class="gulu-button" :class="{[`theme-${theme}`]: theme}">
     <slot />
   </button>
 </template>
@@ -10,13 +8,44 @@ export default {
   props: {
     theme: {
       type: String,
-      default: 'button'
+      default: "button",
     },
   },
 };
 </script>
 <style lang="scss" scoped>
-div {
-  border: 1px solid red;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.gulu-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
 }
 </style>
