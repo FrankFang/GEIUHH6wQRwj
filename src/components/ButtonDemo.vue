7860294a9db45e5aa29e9c315253e6129fb28e31
frankfang <frankfang1990@gmail.com>
2020-08-09 05:58:57 +0800

    添加 click 示例

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick">你好</Button>
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
