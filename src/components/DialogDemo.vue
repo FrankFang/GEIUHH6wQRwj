b371e76eab1d783e92830a84f5fa9f65d7c59859
frankfang <frankfang1990@gmail.com>
2020-08-24 14:57:38 +0800

    Dialog 支持 visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,30 @@
 <template>
 <div>Dialog 示例</div>
 <h1>示例1</h1>
-<Dialog></Dialog>
+<Button @click="toggle">toggle</Button>
+<Dialog v-model:visible="x"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {
+  ref
+} from 'vue'
 export default {
   components: {
-    Dialog
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return {
+      x,
+      toggle
+    }
   }
 }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,23 +1,34 @@
 <template>
-<div class="gulu-dialog-overlay"></div>
-<div class="gulu-dialog-wrapper">
-  <div class="gulu-dialog">
-    <header>标题 <span class="gulu-dialog-close"></span></header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button level="main">OK</Button>
-      <Button>Cancel</Button>
-    </footer>
+<template v-if="visible">
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-wrapper">
+    <div class="gulu-dialog">
+      <header>
+        标题
+        <span class="gulu-dialog-close"></span>
+      </header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <Button level="main">OK</Button>
+        <Button>Cancel</Button>
+      </footer>
+    </div>
   </div>
-</div>
+</template>
 </template>
 
 <script lang="ts">
 import Button from "./Button.vue";
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
   components: {
     Button,
   },
