148dc224a5b5653f08adfc61678a380f5c38689b
frankfang <frankfang1990@gmail.com>
2020-08-24 10:16:32 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Dialog 示例</div>
+<div>Dialog 示例</div>
+<h1>示例1</h1>
+<Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: {
+    Dialog
+  }
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="dialog-overlay"></div>
+<div class="dialog-wrapper">
+  <header>标题</header>
+  <main>
+    <p>第一行字</p>
+    <p>第二行字</p>
+  </main>
+  <footer>
+    <Button>OK</Button>
+    <Button>Cancel</Button>
+  </footer>
+</div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+  components: {
+    Button
+  }
+}
+</script>
