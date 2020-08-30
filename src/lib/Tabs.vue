3fdad19c5299bbd6cb447b7904a62b238baaa7bf
frankfang <frankfang1990@gmail.com>
2020-08-31 00:08:34 +0800

    使用 CSS 切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,8 +4,8 @@
     <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
   </div>
   <div class="gulu-tabs-content">
-    {{current}}
-    <component class="gulu-tabs-content-item" :is="current" />
+    <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+
   </div>
 </div>
 </template>
@@ -78,6 +78,14 @@ $border-color: #d9d9d9;
 
   &-content {
     padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
