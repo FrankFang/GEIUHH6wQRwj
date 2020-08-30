41accdf0089149bd3957524c6e3a7b68effc9e09
frankfang <frankfang1990@gmail.com>
2020-08-30 16:44:55 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-<div>
-  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-  <component v-for="(c,index) in defaults" :is="c" :key="index" />
+<div class="gulu-tabs">
+  <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+  </div>
+  <div class="gulu-tabs-content">
+    <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+  </div>
 </div>
 </template>
 
@@ -25,3 +29,35 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
