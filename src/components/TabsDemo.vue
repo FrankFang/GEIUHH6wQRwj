fb3b9cf23c48d37c6346015cb94f0dc8862e9058
frankfang <frankfang1990@gmail.com>
2020-09-02 02:49:41 +0800

    update

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
 <h1>示例1</h1>
 <Tabs v-model:selected="x">
   <Tab title="导航1">内容1</Tab>
-  <Tab title="导航2222222">内容2</Tab>
+  <Tab title="导航2">内容2</Tab>
 </Tabs>
 </template>
 
@@ -19,7 +19,7 @@ export default {
     Tab
   },
   setup() {
-    const x = ref('导航2222222')
+    const x = ref('导航2')
     return {
       x
     }
