c1f8a02c784e8046212c71442916031a36121d25
frankfang <frankfang1990@gmail.com>
2020-09-10 06:05:50 +0800

    添加首页导航

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="topnav">
-  <div class="logo">
+  <router-link to="/" class="logo">
     <svg class="icon">
       <use xlink:href="#icon-king"></use>
     </svg>
-  </div>
+  </router-link>
   <ul class="menu">
     <li>
       <router-link to="/doc">文档</router-link>
