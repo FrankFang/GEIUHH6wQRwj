8b3d7a2787b732eed165e608e8be3d9ddddabd90
frankfang <frankfang1990@gmail.com>
2020-09-10 05:46:47 +0800

    实现圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,6 +41,7 @@ $color: #007974;
 
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 
 .features {
