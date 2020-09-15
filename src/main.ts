c186602b0cc9d6e151c9c552c5481ce3a596967b
frankfang <frankfang1990@gmail.com>
2020-09-15 13:58:54 +0800

    引入 github 样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import './lib/svg.js'
+import 'github-markdown-css'
 
 const app = createApp(App);
 app.use(router);
