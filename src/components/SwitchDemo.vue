c949a9dc6a836a877466cade9f1320520607d127
frankfang <frankfang1990@gmail.com>
2020-09-17 04:46:48 +0800

    展示源代码并且高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>{{Switch1Demo.__sourceCode}}</pre>
+      <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
     </div>
   </div>
   <div class="demo">
@@ -22,7 +22,7 @@
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>{{Switch2Demo.__sourceCode}}</pre>
+      <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')" />
     </div>
   </div>
 </div>
@@ -33,6 +33,9 @@ import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
+import 'prismjs';
+import 'prismjs/themes/prism.css'
+const Prism = (window as any).Prism
 import {
   ref
 } from 'vue'
@@ -45,7 +48,8 @@ export default {
     return {
       bool,
       Switch1Demo,
-      Switch2Demo
+      Switch2Demo,
+      Prism
     }
   }
 }
