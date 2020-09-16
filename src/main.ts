55d26cf82711d4276550dc4c4d3dea465661e9a8
frankfang <frankfang1990@gmail.com>
2020-09-16 15:54:26 +0800

    事不过三：优化 markdown

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import App from "./App.vue";
 import { router } from "./router";
 import './lib/svg.js'
 import 'github-markdown-css'
+import Markdown from './components/Markdown.vue';
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+app.component("Markdown", Markdown)
