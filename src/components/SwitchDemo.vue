ce1db6397cc27130738c5f58594ca0e94d3c99ac
frankfang <frankfang1990@gmail.com>
2020-09-17 04:17:21 +0800

    抽离用户需要拷贝的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,19 +4,19 @@
   <div class="demo">
     <h2>常规用法</h2>
     <div class="demo-component">
-      <Switch v-model:value="bool" />
+      <Switch1Demo />
     </div>
     <div class="demo-actions">
       <Button>查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
     </div>
   </div>
   <div class="demo">
     <h2>支持 disabled </h2>
     <div class="demo-component">
-      <Switch v-model:value="bool" disabled />
+      <Switch2Demo />
     </div>
     <div class="demo-actions">
       <Button>查看代码</Button>
@@ -31,11 +31,15 @@
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
+import Switch1Demo from './Switch1.demo.vue'
+import Switch2Demo from './Switch2.demo.vue'
 import {
   ref
 } from 'vue'
 export default {
   components: {
+    Switch1Demo,
+    Switch2Demo,
     Switch,
     Button
   },
