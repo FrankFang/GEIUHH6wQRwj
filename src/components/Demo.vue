135abe270004f24a49d3480a720f28ae38e5d4da
frankfang <frankfang1990@gmail.com>
2020-09-23 04:58:32 +0800

    展示或隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
     <component :is="component" />
   </div>
   <div class="demo-actions">
-    <Button @click="toggleCode">查看代码</Button>
+    <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+    <Button @click="showCode" v-else>查看代码</Button>
   </div>
   <div class="demo-code" v-if="codeVisible">
     <pre class="language-html" v-html="html" />
@@ -34,13 +35,15 @@ export default {
     const html = computed(() => {
       return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
     })
-    const toggleCode = () => codeVisible.value = !codeVisible.value
+    const showCode = () => codeVisible.value = true
+    const hideCode = () => codeVisible.value = false
     const codeVisible = ref(false)
     return {
       Prism,
       html,
       codeVisible,
-      toggleCode
+      showCode,
+      hideCode
     }
   }
 }
