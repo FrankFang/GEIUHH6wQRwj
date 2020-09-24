0cfc97357b83bd9eb41953b67a7a0741c76f0929
frankfang <frankfang1990@gmail.com>
2020-09-24 15:50:30 +0800

    部署到码云

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:FrankFang/gulu-ui-website-1.git &&
+git remote add origin git@gitee.com:frankfang/gulu-ui-website-1.git &&
 git push -f -u origin master &&
 cd -
 echo https://fangyinghang.com/gulu-ui-website-1/index.html
