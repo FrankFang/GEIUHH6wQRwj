7075c9485f0743076f1532c1f7542d8fcfa6f27c
frankfang <frankfang1990@gmail.com>
2020-09-24 15:50:13 +0800

    使用 script setup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <router-view />
+<router-view />
 </template>
 
-<script lang="ts">
-import { ref, provide } from "vue";
-import { router } from "./router";
+<script lang="ts" setup>
+import {
+  ref,
+  provide
+} from "vue";
+import {
+  router
+} from "./router";
 
-export default {
-  name: "App",
-  setup() {
-    const width = document.documentElement.clientWidth;
-    const menuVisible = ref(width <= 500 ? false : true);
-    provide("menuVisible", menuVisible); // set
-    router.afterEach(() => {
-      if (width <= 500) {
-        menuVisible.value = false;
-      }
-    });
-  },
-};
+const width = document.documentElement.clientWidth;
+const menuVisible = ref(width <= 500 ? false : true);
+provide("menuVisible", menuVisible); // set
+router.afterEach(() => {
+  if (width <= 500) {
+    menuVisible.value = false;
+  }
+});
 </script>
