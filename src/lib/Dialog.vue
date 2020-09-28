50d7cc6b88372474f1459f49737005b01a144bb1
frankfang <frankfang1990@gmail.com>
2020-09-28 13:34:50 +0800

    将 ok 重命名为 onClickOk，将 cancel 重命名为 onClickCancel 因为 Vue 3 bug

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -12,8 +12,8 @@
           <slot name="content" />
         </main>
         <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
+          <Button level="main" @click="onClickOk">OK</Button>
+          <Button @click="onClickCancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -60,12 +60,12 @@ export const onClickOverlay = () => {
     close()
   }
 }
-export const ok = () => {
+export const onClickOk = () => {
   if (props.ok?.() !== false) {
     close()
   }
 }
-export const cancel = () => {
+export const onClickCancel = () => {
   props.cancel?.()
   close()
 }
