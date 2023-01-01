44e540bb18cb0513d2e9235f24627f002b651e6d
frankfang <frankfang1990@gmail.com>
2023-01-01 19:53:06 +0800

    给 el 添加类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,8 +2,8 @@
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav" ref="container">
       <div class="gulu-tabs-nav-item" v-for="(t, index) in titles"
-        :ref="el => { if (t === selected) selectedItem = el }" @click="select(t)" :class="{ selected: t === selected }"
-        :key="index">{{ t }}</div>
+        :ref="(el: HTMLDivElement) => { if (t === selected) selectedItem = el }" @click="select(t)"
+        :class="{ selected: t === selected }" :key="index">{{ t }}</div>
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
@@ -23,7 +23,7 @@ import {
 } from 'vue'
 
 /** References: https://vitejs.dev/guide/features.html#typescript */
-import type {Component} from 'vue'
+import type { Component } from 'vue'
 
 const props = defineProps<{ selected: string }>()
 const emit = defineEmits<{
